b1110e472b0f1a51fc0e822b5fc2295590ed8766
Julieta Sanchez Mendoza <91549989+JulySanMen@users.noreply.github.com>
2024-12-02 20:15:04 -0600

    grid

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from gridsearch import perform_grid_search  # Importar la función de model.py
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "API de búsqueda de hiperparámetros está activa."
+
+@app.route('/gridsearch', methods=['POST'])
+def grid_search():
+    try:
+        # Llamar a la función del modelo para realizar la búsqueda
+        top_results = perform_grid_search()
+        return jsonify(top_results)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
